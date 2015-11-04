1394208eeea81f7f92f88b9ffbbbd0a7c3ec7849
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-11-04 19:02:13 +0200

    upd

--- site/js/models.js
+++ site/js/models.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
 
+	var Song = Backbone.Model.extend({
+		defaults: {
+			url: '',
+			title: 'untitled'
+		}
+	});
+
+	var Album = Backbone.Collection.extend({
+		model: Song
+	});
+
+	var song1 = new Song({
+		url: 'http://test.soyer.com.ua/Bi-2_(feat._Yu_._Chicherina)_-_Moi_Rok-N-Roll_.mp3',
+		title: 'Bi-2_(feat._Yu_._Chicherina)_-_Moi_Rok-N-Roll'
+	});
+	var song2 = new Song({
+		url: 'http://test.soyer.com.ua/Korol_I_Shut_-_Kukla_Kolduna.mp3',
+		title: 'Korol_I_Shut_-_Kukla_Kolduna'
+	});
+	var song3 = new Song({
+		url: 'http://test.soyer.com.ua/Smyslovye_Gallyutsinatsii_-_Vechno_Molodoi.mp3',
+		title: 'Smyslovye_Gallyutsinatsii_-_Vechno_Molodoi'
+	});
+	var song4 = new Song({
+		url: 'http://test.soyer.com.ua/Tantsy_Minus_-_Gorod.mp3',
+		title: 'Tantsy_Minus_-_Gorod'
+	});
+
+	var myAlbum = new Album();
+
+	myAlbum.add([song1, song2, song3, song4]);
+
+	console.log(myAlbum.toJSON());
+
+
 	
 });
--- src/js/view.js
+++ src/js/view.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
 
-	
+	// header view
+
+	HeaderView = Backbone.View.extend({
+		initialize: function() {
+			this.render();
+		},
+		render: function() {
+			// compile template via underscore
+			var template = _.template( $('#header_template').html(), {} );
+			// load HTML into "el"
+			this.$el.html(template);
+		}
+	});
+
+	var header_view = new HeaderView({
+		el: $('#header')
+	});
+
+	// footer view
+
+	FooterView = Backbone.View.extend({
+		initialize: function() {
+			this.render();
+		},
+		render: function() {
+			// compile template via underscore
+			var template = _.template( $('#footer_template').html(), {} );
+			// load HTML into "el"
+			this.$el.html(template);
+		}
+	});
+
+	var footer_view = new FooterView({
+		el: $('#footer')
+	});
+
 });
