855d0e2cc0dd975352af3f8aa423834b688c15ae
Vadim Graboviy <hraboviyvadim@gmail.com>
2015-11-05 18:14:25 +0200

    preloading

--- site/js/app.js
+++ site/js/app.js
@@ -83,19 +83,36 @@ $(document).ready(function() {
 	};
 	
 	// player
+	$('.js-player').each(function() {
+			$(this)[0].preload="none";
+		});
 	
 	$(document).on('click', '.player-btn', function() {
 		var player = $(this).closest('.controls').find('.js-player')[0];
-	
+		
 		if (player.paused) {
 			// pause all other songs when play new
 			$('.js-player').each(function() {
 				$(this)[0].pause();
+				$(this)[0].currentTime = 0;
 			});
 			$('.player-btn').removeClass('pause').addClass('play');
 	
-			player.play();
+	
 			$(this).removeClass('play').addClass('pause');
+	
+			player.addEventListener('loadstart', function() {
+			    
+			    $(this).next('.loader').addClass('is-visible');
+			    console.log('start loading song');
+			});
+			player.addEventListener('oncanplaythrough', function() {
+				
+			    $(this).next('loader').removeClass('is-visible');
+			    console.log('finished loading song');
+			});
+			player.play();
+			
 			console.log('play');
 		}
 		else {
